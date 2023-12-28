ac552ca5e7287b9161aedc28bebdb20b91360015
phoenixyork166 <berlinlee2017@gmail.com>
2023-12-29 00:33:38 +0800

    basics-assignment-1-start

---
+++ basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrl: './success-alert.component.scss'
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+}
---
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrl: './warning-alert.component.scss'
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+
+  }
+  
+}
